26f517fa2142dbf18a71f38d7e72a1dcca4d0165
Wasfi <mwasfi@hawk.iit.edu>
2017-11-13 21:09:33 -0600

    Re-intreduce the dollar-sign scoped to self-excuting function

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,7 +1,10 @@
 // Release the dollar-sign from jQuery's control
 $.noConflict();
 
-function ringDoorbell() {
+// Re-introduce dollar sign scoped to self-executing
+// function
+(function($) {
   var doorbell = new Audio('media/doorbell.mp3');
+  $('html').removeClass('nojs').addClass('js');
   doorbell.play();
 }
